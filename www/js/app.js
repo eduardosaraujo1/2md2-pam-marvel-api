8626911e4bf36266b36a2e40568d26e59a7de4ef
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2024-08-25 23:47:10 -0300

    Swipe gestures form and result

--- www/js/app.js
+++ www/js/app.js
@@ -1,21 +1,27 @@
-async function loadTree() {
-    const container = document.querySelector(".result-container");
-    let url = "https://gateway.marvel.com";
-    switch (document.querySelector("#paramType").value) {
-        case "characters": url += "/v1/public/characters"
+function swipe(bool) {
+    const content = document.querySelector('.content');
+    if (bool == undefined) {
+        content.classList.toggle('swiped');
+    } else if (bool) {
+        content.classList.add('swiped');
+    } else {
+        content.classList.remove('swiped');
     }
-    const limit = document.querySelector("#paramLimit").value;
-    const offset = document.querySelector("#paramOffset").value;
-    const partName = document.querySelector("#paramNameStartsWith").value;
-
-    const oUrl = new URL(url);
-    oUrl.searchParams.set("limit", limit)
-    oUrl.searchParams.set("offset", offset)
-    oUrl.searchParams.set("nameStartsWith", partName)
-
-    const response = await MarvelAPI.callUrl(oUrl.toString(), "e564c68db076bb8698379be85477a438", "62e3cd60de814d8957740a6dadcbabe976a2ce91")
-    jsonTree.create(response, container);
 }
 
-const executeButton = document.querySelector("#btnExecutar");
-executeButton.addEventListener("click", loadTree);
+let touchStartPosX = 0;
+let touchEndPosX = 0;
+function loadSwipeHandler() {
+    document.addEventListener('touchstart', (event) => {
+        touchStartPosX = event.changedTouches[0].screenX;
+    });
+    document.addEventListener('touchend', (event) => {
+        touchEndPosX = event.changedTouches[0].screenX;
+        const diff = touchEndPosX - touchStartPosX;
+        if (Math.abs(diff) > 100) {
+            swipe(true);
+        }
+    });
+}
+
+loadSwipeHandler();
