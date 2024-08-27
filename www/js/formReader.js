ffc0798030b3decad4d22944cfa6c1b3a7c8d145
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2024-08-27 02:55:40 -0300

    Added form reader

--- www/js/formReader.js
+++ www/js/formReader.js
@@ -1,11 +1,67 @@
-/* 
-TODO:
-- PRIMEIRO ORGANIZE OS DADOS DO FORM PARA BATER COM OS IDS DO MARVELAPI
-- Pegar os dados do form (api FormData()) e salvar em objeto
-- Pegar os dados das inputLists, e converter em lista de números para cada categoria
-- Remover os dados que estão como "ignore" ou vazios
-- Transformar esses dados numa string propria para URL
-- Autenticar essa URL e enviar request
-- Fazer swipe e exibir animação de carregamento
-- Quando request responder, seja OK ou erro, exibir usando jsonTree
-*/
+const formReader = (function () {
+    function readBasic() {
+        const form = document.querySelector('form#api-form');
+        const formData = new FormData(form);
+        const data = Object.fromEntries(formData.entries());
+        return data;
+    }
+
+    function readInputLists() {
+        const form = document.querySelector('form#api-form');
+        const inputLists = form.querySelectorAll('.input-list');
+        const data = {};
+
+        for (const list of inputLists) {
+            let inputs = [...list.querySelectorAll('input')];
+            data[list.id] = inputs.map((i) => i.value);
+        }
+        return data;
+    }
+
+    function readFormData() {
+        const basicData = readBasic();
+        const advancedData = readInputLists();
+        const data = { ...basicData, ...advancedData };
+        return data;
+    }
+
+    /**
+     * Removes empty string and arrays from the object
+     * @param {object} object Object to remove empty entries
+     */
+    function removeEmptyEntries(object) {
+        if (typeof object !== 'object' || object === null) {
+            return object;
+        }
+
+        for (const key in object) {
+            const value = object[key];
+
+            // remove empty string
+            if (typeof value === 'string' && value === '') {
+                delete object[key];
+            }
+
+            // remove all empty arrays, ignoring empty strings
+            if (Array.isArray(value)) {
+                const filtered = value.filter((e) => e !== '');
+                if (filtered.length === 0) {
+                    delete object[key];
+                }
+            }
+        }
+        return object;
+    }
+    return {
+        /**
+         *
+         * @returns Formatted form data to generate the request url
+         */
+        read: () => {
+            let data;
+            data = readFormData();
+            data = removeEmptyEntries(data);
+            return data;
+        },
+    };
+})();
